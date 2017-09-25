ce4757e2076cbfa433211348b43456f856f992a9
Martin Bäumer <martin.baeumer@gmail.com>
2017-09-25 21:14:47 +0200

    working on file structure

---
+++ src/main/java/se/squeed/secu/ParkApplication.java
@@ -0,0 +1,12 @@
+package se.squeed.secu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkApplication.class, args);
+	}
+}
