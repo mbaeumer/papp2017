2f1c6780e22fdf22d8f9840e9804f21e8e926438
Martin Bäumer <martin.baeumer@gmail.com>
2017-09-25 22:05:42 +0200

    working on file structure

---
+++ src/main/java/se/squeed/secu/models/Credentials.java
@@ -0,0 +1,25 @@
+package se.squeed.secu.models;
+
+/**
+ * Created by martinbaumer on 11/08/16.
+ */
+public class Credentials {
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
