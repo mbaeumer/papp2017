098eb32c53b5e63689c670e3996b1ea35a0cc662
Martin Bäumer <martin.baeumer@gmail.com>
2017-10-08 21:17:23 +0200

    prepare for deploy

--- src/main/java/se/squeed/secu/repositories/InspectionRepository.java
+++ src/main/java/se/squeed/secu/repositories/InspectionRepository.java
@@ -7,12 +7,14 @@ import se.squeed.secu.models.Inspection;
 import se.squeed.secu.models.User;
 
 import javax.persistence.TemporalType;
+import javax.transaction.Transactional;
 import java.util.Date;
 import java.util.List;
 
 /**
  * Created by martinbaumer on 27/09/17.
  */
+@Transactional
 public interface InspectionRepository extends JpaRepository<Inspection, String> {
     List<Inspection> findInspectionsByUserAndInspectionDateOrderByEndTimeDesc(User user, @Temporal(TemporalType.DATE)  Date date);
     Inspection findById(int id);
