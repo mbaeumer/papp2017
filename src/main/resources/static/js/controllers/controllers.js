599abb5c134dc1ee495f4c970354ba64351e166a
Martin Bäumer <martin.baeumer@gmail.com>
2017-10-10 23:44:28 +0200

    clean up cotroller

--- src/main/resources/static/js/controllers/controllers.js
+++ src/main/resources/static/js/controllers/controllers.js
@@ -6,14 +6,6 @@ app.controller('appController',function($scope, $location, cookieUtilService){
 
     $scope.isAdmin = function () {
         return $scope.isLoggedIn() && cookieUtilService.getUserType() != '1';
-        /*
-        usertype = cookieUtilService.getUserType();
-        if (usertype === undefined){
-          var isUndef = true;
-        }
-        result = cookieUtilService.getUserType() !== undefined && usertype != '1';
-        return result;
-        */
     };
 
     $scope.isLoggedIn = function(){
@@ -469,26 +461,6 @@ app.controller('summaryController',function($scope, $location, summaryService, e
 
     }
 
-/*
-    $scope.updateCustomDateFilter = function(){
-    	var date = new Date($scope.customDate);
-	   	var strDate = date.toString("MM/dd/yyyy");
-
-	   	var startDate = new Date(strDate);
-    	var endDate = new Date();
-
-    	$scope.startDate = new Date(startDate.getTime());
-		endDate = new Date(startDate.getTime());
-		$scope.endDate = new Date(endDate.addDays(1));
-
-		fileService.startDate = $scope.startDate.toString("MM/dd/yyyy");
-    	fileService.endDate = $scope.endDate.toString("MM/dd/yyyy");
-    	filterService.startDate = $scope.startDate.toString("MM/dd/yyyy");
-    	filterService.endDate = $scope.endDate.toString("MM/dd/yyyy");
-
-    	//$scope.metaSummary = metaSummaryService.getMetaSummary($scope.successMetaSummaryCallback, $scope.errorMetaSummaryCallback);
-    };
-*/
     $scope.deleteSummary = function (id) {
         inspectionService.getSingleInspection(id, $scope.successReadSingleForDeletionCallback, $scope.errorReadSingleCallback);
     };
@@ -562,55 +534,17 @@ app.controller('summaryController',function($scope, $location, summaryService, e
             $scope.summaryParam.fromDate.setSeconds(00);
             summaryService.getSummaries($scope.summaryParam, $scope.successCallback, $scope.errorCallback);
     	}
-    	/*
-    	else if (period === 'past_week'){
-    		var d = new Date(dateString);
-    		var day = new Date(d.getDay());
-    		var diff = new Date(d.getDate() - day + (day == 0 ? -6:1));
-    		var endDate = new Date(d.getTime());
-    		d = new Date(d.setDate(diff));
-    		$scope.startDate = new Date(d);
-    		$scope.endDate = new Date(endDate.addDays((8 - day))).addMinutes(-1);
-    		$scope.startDate = new Date($scope.startDate.getTime()).addDays(-7);
-    		$scope.endDate = new Date($scope.endDate.getTime()).addDays(-7);
-    	}else if (period === 'this_week'){
-    		var d = new Date(dateString);
-    		var day = new Date(d.getDay());
-    		var diff = new Date(d.getDate() - day + (day == 0 ? -6:1));
-    		var endDate = new Date(d.getTime());
-    		d = new Date(d.setDate(diff));
-    		$scope.startDate = new Date(d);
-    		$scope.endDate = new Date(endDate.addDays((8 - day))).addMinutes(-1);
-    	}else if (period === 'past_month'){
-    		var date = new Date(dateString);
-    		var firstDay = new Date(date.getFullYear(), date.getMonth() - 1, 1);
-    		var lastDay = new Date(date.getFullYear(), date.getMonth(), 0);
-    		$scope.startDate = firstDay;
-    		$scope.endDate = lastDay.addDays(1);
-    	}else if (period === 'this_month'){
-    		var date = new Date(dateString);
-    		var firstDay = new Date(date.getFullYear(), date.getMonth(), 1);
-    		var lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0);
-    		$scope.startDate = firstDay;
-    		$scope.endDate = lastDay.addDays(1);
-    	}else{
-    		$scope.startDate = "";
-    		$scope.endDate = "";
-    	}
-    	*/
 
     	fileService.startDate = $scope.summaryParam.fromDate;
     	fileService.endDate = $scope.summaryParam.toDate;
     	filterService.startDate = $scope.summaryParam.fromDate;
     	filterService.endDate = $scope.summaryParam.toDate;
-
-    	//$scope.metaSummary = metaSummaryService.getMetaSummary($scope.successMetaSummaryCallback, $scope.errorMetaSummaryCallback);
     };
 
     $scope.successMetaSummaryCallback = function(data){
     	$scope.metaSummary = data;
-    	filterService.startDate = "";//$scope.startDate.toString("MM/dd/yyyy");
-    	filterService.endDate = "";//$scope.endDate.toString("MM/dd/yyyy");
+    	filterService.startDate = "";
+    	filterService.endDate = "";
     };
 
     $scope.errorMetaSummaryCallback = function(message){
@@ -622,256 +556,6 @@ app.controller('summaryController',function($scope, $location, summaryService, e
 
     fileService.startDate = $scope.summaryParam.fromDate;
     fileService.endDate = $scope.summaryParam.toDate;
-
-
-    //$scope.metaSummary = metaSummaryService.getMetaSummary($scope.successMetaSummaryCallback, $scope.errorMetaSummaryCallback);
 });
-
-
-
-
-/*
-function activityTypeController($scope, $location, ActivityType, entityService, loginService){
-
-	if (loginService.currentUserId === 0){
-		$location.path("/home");
-	}
-	
-    $scope.editActivityType = function (id) {
-        $scope.entity = ActivityType.get({"typeId": id }, function(topic, getResponseHeaders){
-        	entityService.activityToEdit = $scope.entity;
-        	$location.path("/editActivity");
-        });
-    };
-    
-    $scope.activityTypes = ActivityType.query();
-};
-
-function createActivityTypeController($scope, $http, $location, ActivityType, activityTypeService, loginService){
-    $scope.entity = { code: '', description: ''};
-    
-    if (loginService.currentUserId === 0){
-		$location.path("/home");
-	}
-    
-    $scope.title = "Registrera ny aktivitetstyp";
-
-    $scope.saveActivityType =  function(){
-        var activityType = new ActivityType();
-		activityType.code = $scope.entity.code;
-    	activityType.description = $scope.entity.description;
-    	
-    	activityTypeService.createActivityType(activityType, $scope.successCallback, $scope.errorCallback);   	
-    };
-	
-    $scope.abortActivityTypeCreation = function(){
-    	$location.path("/activityTypes");
-    };  
-    
-    $scope.successCallback = function(){
-        $location.path("/activityTypes");
-    };
-
-    $scope.errorCallback = function(message){
-        $scope.errorMessage = message;
-    };
-};
-
-function editActivityTypeController($scope, $http, loginService, entityService, activityTypeService, $location){
-	$scope.entity = entityService.activityToEdit;
-	$scope.title = "Redigera aktivitetstyp";
-
-	if (loginService.currentUserId === 0){
-		$location.path("/home");
-	}
-	
-	$scope.saveActivityType =  function(){
-    	var activity = $scope.entity;  	
-    	activity.description = $scope.entity.description;
-    	activity.code = $scope.entity.code;
-    	activityTypeService.editActivityType(activity, $scope.successCallback, $scope.errorCallback);
-    };
-	
-    $scope.abortActivityTypeCreation = function(){
-    	$location.path("/activityTypes");
-    };
-    $scope.successCallback = function(){
-        $location.path("/activityTypes");
-    };
-
-    $scope.errorCallback = function(message){
-        $scope.errorMessage = message;
-    };
-};
-
-
-function editUserController($scope, $http, $location, loginService, User, UserType, entityService, userService){
-	$scope.entity = entityService.userToEdit;
-	
-	$scope.title = "Redigera användare";
-	$scope.roles = UserType.query(function(){
-		$scope.selectedRole = $scope.roles[$scope.entity.userType.id-1];
-    });
-	
-	if (loginService.currentUserId === 0){
-		$location.path("/home");
-	}
-	
-	$scope.saveUser =  function(){
-    	var user = $scope.entity;
-    	user.name = $scope.entity.name;
-    	user.usercode = $scope.entity.usercode;
-    	user.userType = { id: $scope.selectedRole.id, name: $scope.selectedRole.name};
-    	userService.editUser(user, $scope.successCallback, $scope.errorCallback);
-    };
-
-    $scope.abortUserCreation = function(){
-    	$location.path("/users");
-    };    
-
-    $scope.successCallback = function(){
-        $location.path("/users");
-    };
-
-    $scope.errorCallback = function(message){
-        $scope.errorMessage = message;
-    };
-};
-
-
-function categoryTypeController($scope, $location, Category, entityService, loginService){
-
-	if (loginService.currentUserId === 0){
-		$location.path("/home");
-	}
-	
-    $scope.editCategoryType = function (id) {
-        $scope.entity = Category.get({"typeId": id }, function(topic, getResponseHeaders){
-        	entityService.categoryToEdit = $scope.entity;
-        	$location.path("/editCategory");
-        });
-    };
-
-    $scope.categoryTypes = Category.query();
-};
-
-function createCategoryTypeController($scope, $http, categoryTypeService, Category, $location, loginService){
-    $scope.entity = { code: '', description: ''};
-    
-    if (loginService.currentUserId === 0){
-		$location.path("/home");
-	}
-
-    $scope.title = "Registrera ny kategorityp";
-    
-    $scope.saveCategoryType =  function(){
-    	var category = new Category();
-    	category.code = $scope.entity.code;
-    	category.description = $scope.entity.description;    	
-    	categoryTypeService.createCategoryType(category, $scope.successCallback, $scope.errorCallback);
-    };
-	
-    $scope.abortCategoryTypeCreation = function(){
-    	$location.path("/categoryTypes");
-    };   
-    
-    $scope.successCallback = function(){
-        $location.path("/categoryTypes");
-    };
-
-    $scope.errorCallback = function(message){
-        $scope.errorMessage = message;
-    };
-};
-
-function editCategoryTypeController($scope, $http, loginService, Area, entityService, categoryTypeService, $location){
-	$scope.entity = entityService.categoryToEdit;	
-	$scope.title = "Redigera kategorityp";
-
-	if (loginService.currentUserId === 0){
-		$location.path("/home");
-	}
-	
-	$scope.saveCategoryType =  function(){
-    	var category = $scope.entity;  	
-    	category.description = $scope.entity.description;
-    	category.code = $scope.entity.code;
-    	categoryTypeService.editCategoryType(category, $scope.successCallback, $scope.errorCallback);
-    };
-	
-    $scope.abortCategoryTypeCreation = function(){
-    	$location.path("/categoryTypes");
-    };
-	
-    $scope.successCallback = function(){
-        $location.path("/categoryTypes");
-    };
-
-    $scope.errorCallback = function(message){
-        $scope.errorMessage = message;
-    };
-};
-
-function fileController($scope, $location, loginService, Area, entityService, fileService){
-
-	if (loginService.currentUserId === 0){
-		//$location.path("/home");
-	}
-	  
-	$scope.editArea = function (id) {
-        $scope.entity = Area.get({"areaId": id }, function(topic, getResponseHeaders){
-        	entityService.areaToEdit = $scope.entity;
-        	$location.path("/editArea");
-        });
-    };
-    
-    $scope.successCallback = function(resource, url){
-    	window.open(url);
-        $location.path("/summary");
-    };
-
-    $scope.errorCallback = function(message){
-        $scope.errorMessage = message;
-    };
-    
-    fileService.getFile($scope.successCallback, $scope.errorCallback);
-};
-
-
-function profileController($scope, $location, entityService, loginService, User, UserType, userService){
-
-	if (loginService.currentUserId === 0){
-		$location.path("/home");
-	}
-	
-	$scope.entity = User.get({"userId": loginService.currentUserId }, function(topic, getResponseHeaders){
-		$scope.roles = UserType.query(function(){
-			$scope.selectedRole = $scope.roles[$scope.entity.userType.id-1];
-	    });
-    });	
-	
-	$scope.saveProfile = function (viewLocation) {
-        var user = $scope.entity;
-        userService.editUser(user, $scope.successCallback, $scope.errorCallback);
-    };
-    
-    $scope.successCallback = function(){
-        $location.path("/inspections");
-    };
-
-    $scope.errorCallback = function(message){
-        $scope.errorMessage = message;
-    };
-    
-    $scope.abortProfileUpdate = function(){
-    	$location.path("/inspections");
-    };
-};
-*/
-
-
-
-
-
 
 
