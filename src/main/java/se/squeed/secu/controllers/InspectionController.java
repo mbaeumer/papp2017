c7b7bb30c0880600df0cbb007ed29721674d0974
Martin Bäumer <martin.baeumer@gmail.com>
2017-10-11 20:46:23 +0200

    fixing the filter

--- src/main/java/se/squeed/secu/controllers/InspectionController.java
+++ src/main/java/se/squeed/secu/controllers/InspectionController.java
@@ -9,7 +9,10 @@ import se.squeed.secu.repositories.InspectionRepository;
 import se.squeed.secu.util.ValidationResult;
 import se.squeed.secu.util.ValidationUtils;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -129,12 +132,50 @@ public class InspectionController {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
+    /*
     @RequestMapping(value="summary", method= RequestMethod.POST)
     public MetaSummary getSummaryBetweenDates(@RequestBody SummaryParam summaryParam)  {
         MetaSummary metaSummary = new MetaSummary();
         long totalDuration = 0;
         int totalFined = 0;
         List<Inspection> inspections = inspectionRepository.findAllByInspectionDateBetweenOrderByUserAscInspectionDateDescStartTimeAsc(summaryParam.getFromDate(), summaryParam.getToDate());
+        List<Summary> summaries = new ArrayList<>();
+        for (Inspection inspection : inspections){
+            Summary summary = new Summary();
+            summary.setInspection(inspection);
+            totalDuration += summary.getDuration();
+            totalFined += summary.getFined();
+            summaries.add(summary);
+        }
+        metaSummary.setSummaries(summaries);
+        metaSummary.setTotalFined(totalFined);
+        metaSummary.setTotalTimInMinutes(totalDuration / 60);
+        metaSummary.setTotalDecimalTime();
+        metaSummary.setTotalTimeValue();
+        metaSummary.setAverage(metaSummary.getTotalFined() / metaSummary.getTotalDecimalTime());
+        return metaSummary;
+    }
+    */
+
+    @RequestMapping(value="summary", method= RequestMethod.GET)
+    public MetaSummary getSummaryBetweenDates(@RequestParam String from, @RequestParam String to)  {
+        MetaSummary metaSummary = new MetaSummary();
+
+        SimpleDateFormat formatter = new SimpleDateFormat("EEE MMM dd yyyy HH:mm:ss z");
+        Date fromDate = null;
+        Date toDate = null;
+
+        try {
+            fromDate = formatter.parse(from);
+            toDate = formatter.parse(to);
+
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        long totalDuration = 0;
+        int totalFined = 0;
+        List<Inspection> inspections = inspectionRepository.findAllByInspectionDateBetweenOrderByUserAscInspectionDateDescStartTimeAsc(fromDate, toDate);
         List<Summary> summaries = new ArrayList<>();
         for (Inspection inspection : inspections){
             Summary summary = new Summary();
--- src/main/resources/static/js/services/summaryService.js
+++ src/main/resources/static/js/services/summaryService.js
@@ -2,6 +2,17 @@ var services = angular.module('services');
 services.factory('summaryService',function($http, hostAddressService){
     return {
         getSummaries : function(summaryParam, callbackSuccess, callbackError){
+            var url = hostAddressService.hostAddress + 'inspections/summary';
+            url += '?from=' + summaryParam.fromDate + '&to=' + summaryParam.toDate;
+            $http.get(url).then(function(data){
+                if (data.status == 200){
+                    callbackSuccess(data.data);
+                }
+                else{
+                    callbackError("Error retrieving summary");
+                }
+            });
+            /*
             $http.post(hostAddressService.hostAddress + 'inspections/summary', summaryParam).then(function(data){
                 if (data.status == 200){
                     callbackSuccess(data.data);
@@ -10,6 +21,7 @@ services.factory('summaryService',function($http, hostAddressService){
                     callbackError("Error when retrieving the summaries");
                 }
             });
+            */
         },
         createInspection : function(inspection, callbackSuccess, callbackError){
             $http.post(hostAddressService.hostAddress + 'inspections', inspection).then(function(data){
